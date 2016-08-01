58096d677db54ea672a816777721e68eadbb5fb7
Vũ Xuân Thành <vuthanh20286@gmail.com>
2016-08-02 06:35:50 +0700

    Rename project

---
+++ AutoPostFb/Boundary/frmMainControl.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Boundary.AutoPostFb
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFacebookLogin = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbInfosHolder = new System.Windows.Forms.Label();
+            this.lblUserInfos = new System.Windows.Forms.Label();
+            this.pcbLogo = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFacebookLogin
+            // 
+            this.btnFacebookLogin.Location = new System.Drawing.Point(911, 10);
+            this.btnFacebookLogin.Name = "btnFacebookLogin";
+            this.btnFacebookLogin.Size = new System.Drawing.Size(61, 31);
+            this.btnFacebookLogin.TabIndex = 1;
+            this.btnFacebookLogin.Text = "Login";
+            this.btnFacebookLogin.UseVisualStyleBackColor = true;
+            this.btnFacebookLogin.Click += new System.EventHandler(this.btnFacebookLogin_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(911, 10);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(61, 31);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Visible = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnFacebookLogin);
+            this.panel1.Controls.Add(this.lbInfosHolder);
+            this.panel1.Controls.Add(this.lblUserInfos);
+            this.panel1.Controls.Add(this.pcbLogo);
+            this.panel1.Controls.Add(this.btnLogout);
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(975, 49);
+            this.panel1.TabIndex = 4;
+            // 
+            // lbInfosHolder
+            // 
+            this.lbInfosHolder.AutoSize = true;
+            this.lbInfosHolder.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInfosHolder.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lbInfosHolder.Location = new System.Drawing.Point(768, 17);
+            this.lbInfosHolder.Name = "lbInfosHolder";
+            this.lbInfosHolder.Size = new System.Drawing.Size(99, 17);
+            this.lbInfosHolder.TabIndex = 6;
+            this.lbInfosHolder.Text = "Logged in user";
+            // 
+            // lblUserInfos
+            // 
+            this.lblUserInfos.AutoSize = true;
+            this.lblUserInfos.Location = new System.Drawing.Point(734, 17);
+            this.lblUserInfos.Name = "lblUserInfos";
+            this.lblUserInfos.Size = new System.Drawing.Size(27, 17);
+            this.lblUserInfos.TabIndex = 5;
+            this.lblUserInfos.Text = "Hi: ";
+            // 
+            // pcbLogo
+            // 
+            this.pcbLogo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pcbLogo.Location = new System.Drawing.Point(3, 3);
+            this.pcbLogo.Name = "pcbLogo";
+            this.pcbLogo.Size = new System.Drawing.Size(100, 43);
+            this.pcbLogo.TabIndex = 4;
+            this.pcbLogo.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(3, 57);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(975, 514);
+            this.panel2.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 571);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Auto Post FB";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFacebookLogin;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pcbLogo;
+        private System.Windows.Forms.Label lbInfosHolder;
+        private System.Windows.Forms.Label lblUserInfos;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
